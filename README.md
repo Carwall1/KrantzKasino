a158b692dfeda36ed46b6a2cc0d656f1aadfd61b
matfolkarn <hampus.mattfolk@gmail.com>
2025-04-15 21:50:05 +0200

    swagger added

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 # KrantzKasino
 
-
 Install dependencies for backend:
 
 chmod +x server/setup.sh
 
 ./server/setup.sh
 
-
-
 Star backend:
 
 python server/manage.py runserver
-
 
 Migrate:
 
@@ -20,7 +16,11 @@ python manage.py makemigrations mysite
 
 python manag.py migrate
 
-
 Create superuser:
 
 python mange.py createsuperuser
+
+
+Swagger:
+
+http://127.0.0.1:8000/swagger/
--- server/mysite/views.py
+++ server/mysite/views.py
@@ -2,7 +2,21 @@ from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import UserSerializer
+from rest_framework import generics
+from .models import User
+
 
 @api_view(['GET'])
 def hello(request):
     return Response({"message": "Hello from Django!"})
+
+
+
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
